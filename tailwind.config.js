f3f2236fd63234bf7c779d73774545d4b0d763ac
Brice McNeley <btmac1212@gmail.com>
2024-01-14 12:56:31 -0600

    mobileFinal

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    screens: {
+      'mobileM': '375px',
+      'mobileL': '425px',
+      'sm': '640px',
+      'md': `768px`,
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
